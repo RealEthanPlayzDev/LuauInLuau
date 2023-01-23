4298616c6866ba6c1e5a02ec43cd933072bd3956
RealEthanPlayzDev <radiatedexodus@gmail.com>
2023-01-23 12:21:47 +0700

    Revert any chunk name arg changes
    It breaks Luau.Run (and Luau.RunBytecode)

--- main.cpp
+++ main.cpp
@@ -10,29 +10,29 @@ extern "C" char* LuauCompile(const char* src)
     return luau_compile(src, strlen(src), nullptr, &BytecodeSize);
 }
 
-extern "C" int LuauRunUsingCustomState(lua_State* L, const char* bytecode, const char* chunkname)
+extern "C" int LuauRunUsingCustomState(lua_State* L, const char* bytecode)
 {
     std::string BytecodeStdString = bytecode;
-    int LoadResult = luau_load(L, chunkname, bytecode, BytecodeStdString.size(), 0);
+    int LoadResult = luau_load(L, "LuauInLuau_Chunk", bytecode, BytecodeStdString.size(), 0);
     if (LoadResult == 1) { return 1; }
     return lua_pcall(L, 0, 0, 0);
 }
 
-extern "C" int LuauRun(const char* bytecode, const char* chunkname)
+extern "C" int LuauRun(const char* bytecode)
 {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
-    int Result = LuauRunUsingCustomState(L, bytecode, chunkname);
+    int Result = LuauRunUsingCustomState(L, bytecode);
     lua_close(L);
     return Result;
 }
 
-extern "C" int LuauRunWithSafeEnv(const char* bytecode, const char* chunkname)
+extern "C" int LuauRunWithSafeEnv(const char* bytecode)
 {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
     luaL_sandbox(L);
-    int Result = LuauRunUsingCustomState(L, bytecode, chunkname);
+    int Result = LuauRunUsingCustomState(L, bytecode);
     lua_close(L);
     return Result;
 }
@@ -40,7 +40,7 @@ extern "C" int LuauRunWithSafeEnv(const char* bytecode, const char* chunkname)
 int main()
 {
     const char* Bytecode = LuauCompile("print(\"hello world\")");
-    LuauRun(Bytecode, "LuauInLuau_Chunk");
-    LuauRunWithSafeEnv(Bytecode, "LuauInLuau_Chunk");
+    LuauRun(Bytecode);
+    LuauRunWithSafeEnv(Bytecode);
     return 0;
 }
